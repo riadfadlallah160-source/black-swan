500581e584faca4d6ca9fde0017c58358bdcaa46
Riad <riadfadlallah160@gmail.com>
2026-09-10 23:10:24 +0100

    Harden narrative exclusions and quality sources

--- src/config.js
+++ src/config.js
@@ -7,7 +7,10 @@ export const CONFIG = Object.freeze({
   creatorFeeSplitBps: 8000,
   sniperProtection: true,
   blockedNarrativeTerms: [
-    '9/11','911','september 11','terror','terrorist','massacre','killed','dead','death','bomb','shooting','war casualty','hostage','suicide'
+    '9/11','911','9 11','september 11','world trade center','bin laden','al qaeda',
+    'terror','terrorist','terrorism','massacre','killed','killing','murder','murdered',
+    'dead','death','fatal','bomb','bombing','explosion','shooting','gunman','war casualty',
+    'hostage','suicide','memorial','funeral','assassination','genocide','victim'
   ],
   feeds: {
     googleTrends: [
@@ -18,7 +21,8 @@ export const CONFIG = Object.freeze({
     googleNews: [
       'https://news.google.com/rss/search?q=viral%20meme%20OR%20internet%20trend&hl=en-US&gl=US&ceid=US:en',
       'https://news.google.com/rss/search?q=gaming%20viral%20OR%20technology%20viral&hl=en-US&gl=US&ceid=US:en',
-      'https://news.google.com/rss/search?q=celebrity%20viral%20OR%20sports%20viral&hl=en-US&gl=US&ceid=US:en'
+      'https://news.google.com/rss/search?q=funny%20viral%20OR%20internet%20culture&hl=en-US&gl=US&ceid=US:en',
+      'https://news.google.com/rss/search?q=consumer%20technology%20launch%20OR%20gaming%20launch&hl=en-US&gl=US&ceid=US:en'
     ],
     coinGeckoTrending: 'https://api.coingecko.com/api/v3/search/trending'
   }
